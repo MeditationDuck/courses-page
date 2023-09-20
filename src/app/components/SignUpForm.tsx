5966bbc74b8e56ea46752c3bfaf80e5b4069066a
MeditationDuck <yoshinao@fit.cvut.cz>
2023-09-20 13:58:40 +0900

    feat: signup implemented want to use more feature of next js like toast and so on.

---
+++ src/app/components/SignUpForm.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { useState } from "react";
+import { signUp } from "../actions/users/SignUp";
+
+const SignUpForm = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const [message, setMessage] = useState("")
+
+  const handleSubmit = async () => {
+    setMessage("Signing up...")
+    const message = await signUp(email, password)
+    setMessage(message)
+  }
+
+  return (
+      <div className="gap-4 bg-gray-400 p-4 w-full sm:w-[50%] flex flex-col rounded-md">
+        <input type='text' value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <button onClick={handleSubmit}>
+          Sign Up
+        </button>
+        <p>{message}</p>
+      </div>
+  );
+}
+ 
+export default SignUpForm;
