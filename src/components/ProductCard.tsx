97bb93f15840fe0ba652ed65df43208e7c978b9c
MeditationDuck <yoshinao@fit.cvut.cz>
2023-11-01 00:12:46 +0100

    feat: update Product card design

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -17,31 +17,42 @@ const ProductCard = ({
   const router = useRouter()
   return (
     <div 
-      className='flex flex-row py-4'
+      className='flex flex-col sm:flex-row py-7'
       onClick={() => {router.push(`/products/${data.id}`)}}
     >
+      <div className="flex flex-row">
       <Image
         src={data?.images?.[0]?.url}
         alt="product image"
+        layout="fixed"
+        objectFit="cover"
         width={200}
-        height={100}
+        height={200}
         className={cn("rounded-lg", data.isArchived && "opacity-50")}
       />
-      {/* <div className="flex flex-col flex-row:md py-4 "> */}
+        <div className="sm:hidden flex flex-col p-4 justify-between">
+          <p className=' font-bold text-3xl pt-auto'>$ {data.price}</p>
+          <p className=" text-sm text-muted-foreground">{data.description}</p>
 
-     
-      <div className='p-8 grow justify-between flex flex-col'>
-        <Heading title={data.name} description={data.description} />
-        <p className='font-bold text-xl pt-auto'>$ {data.price}</p>
+          { data.isArchived && (
+            <div className="text-red-400">
+              Currently Not Available.
+            </div>
+          )}
+        </div>
+      </div> 
+      <div className='pt-2 sm:p-8 grow justify-between flex flex-col'>
+        {/* <Heading title={data.name} description={data.description} /> */}
+        <h2 className="text-3xl font-bold tracking-tight">{data.name}</h2>
+        <p className="sm:visible collapse text-sm text-muted-foreground">{data.description}</p>
+        <p className='sm:visible collapse font-bold text-xl pt-auto'>$ {data.price}</p>
      
       { data.isArchived && (
-        <div className="pt-4 text-red-400">
+        <div className="sm:visible collapse text-red-400">
           Currently Not Available.
         </div>
       )}
       </div>
-      {/* </div> */}
-      
     </div>
   );
 }
